00f0a2a831cb446b106508cce0f9ae21aa9d5cac
smilecraft00 <69019569+smilecraft4@users.noreply.github.com>
2024-10-25 21:37:19 +0200

    File dialogs working

--- tests/Prompts.cpp
+++ tests/Prompts.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <windowsx.h>
 
-static COMDLG_FILTERSPEC rgSpec[] = {{TEXT("Mashiro files"), TEXT("*.msh")}};
+static COMDLG_FILTERSPEC rgSpec[] = {{TEXT("Mashiro files"), TEXT("*.msh")}, {TEXT("All files"), TEXT("*.*")}};
 
 static std::filesystem::path SaveAsDialog(std::filesystem::path directory = "./",
                                           std::filesystem::path filename = "unsaved.msh");
@@ -17,16 +17,14 @@ static std::filesystem::path OpenDialog(std::filesystem::path directory = "./");
 class App {
   public:
     App(std::filesystem::path filename, bool saved, bool openned, bool named)
-        : filename(filename), saved(saved), openned(openned), named(named) {
+        : filename(filename), saved(saved), openned(openned), named(named), hwnd(NULL) {
     }
 
     void SaveAs() {
         if (openned) {
-            // SaveAsDialog
-
             // File::Save(filename)
+            filename = SaveAsDialog();
             MessageBox(hwnd, std::format(TEXT("Saved \"{}\""), filename.wstring()).c_str(), TEXT("SaveAs test"), MB_OK);
-            filename = "./unnamed.msh"; // TODO
             saved = true;
             named = true;
             openned = true;
@@ -95,10 +93,8 @@ class App {
             filename = "";
         }
 
-        const auto path = OpenDialog();
-
         // File::Open(path)
-        filename = path;
+        filename = OpenDialog();
         MessageBox(hwnd, std::format(TEXT("Openned file \"{}\""), filename.wstring()).c_str(), TEXT("Open test"),
                    MB_OK);
         openned = true;
@@ -129,8 +125,39 @@ class App {
 };
 
 std::filesystem::path SaveAsDialog(std::filesystem::path directory, std::filesystem::path filename) {
-    const auto filename = directory / filename;
-    return "";
+    if (!std::filesystem::exists(directory) || !std::filesystem::is_directory(directory)) {
+        throw new std::runtime_error("Directory provided as default path for SaveAsDialog does not exists");
+    }
+    directory = std::filesystem::absolute(directory);
+
+    IFileDialog *pfd{};
+    IShellItem *psiResult{};
+    IShellItem *folder{};
+    PWSTR pszFilePath{};
+
+    HRESULT hr = CoCreateInstance(CLSID_FileSaveDialog, NULL, CLSCTX_ALL, IID_PPV_ARGS(&pfd));
+    DWORD dwFlags;
+    hr = pfd->GetOptions(&dwFlags);
+    hr = pfd->SetOptions(dwFlags | FOS_FORCEFILESYSTEM);
+    hr = pfd->SetFileTypes(ARRAYSIZE(rgSpec), rgSpec);
+    hr = pfd->SetFileTypeIndex(1);
+    hr = pfd->SetFileName(filename.wstring().c_str());
+    hr = pfd->SetDefaultExtension(L"msh");
+    hr = SHCreateItemFromParsingName(directory.wstring().c_str(), NULL, IID_PPV_ARGS(&folder));
+    hr = pfd->SetDefaultFolder(folder);
+
+    hr = pfd->Show(NULL);
+    hr = pfd->GetResult(&psiResult);
+    hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+    const auto path = std::filesystem::path(pszFilePath);
+
+    CoTaskMemFree(pszFilePath);
+    folder->Release();
+    psiResult->Release();
+    pfd->Release();
+
+    return path;
 }
 
 std::filesystem::path OpenDialog(std::filesystem::path directory) {
@@ -138,160 +165,36 @@ std::filesystem::path OpenDialog(std::filesystem::path directory) {
         throw new std::runtime_error("Directory provided as default path for OpenDialog does not exists");
     }
 
-    // Make sure the path is absolute
-    if (directory.is_absolute()) {
-        directory = std::filesystem::absolute(directory);
-    }
+    directory = std::filesystem::absolute(directory);
 
-    std::filesystem::path filename;
+    IFileDialog *pfd{};
+    IShellItem *folder{};
+    IShellItem *psiResult{};
+    PWSTR pszFilePath{};
 
-    IFileDialog *pfd = NULL;
-    HRESULT hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pfd));
+    HRESULT hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_PPV_ARGS(&pfd));
 
-    // Set the options on the dialog.
     DWORD dwFlags;
-
-    // Before setting, always get the options first in order
-    // not to override existing options.
     hr = pfd->GetOptions(&dwFlags);
-
-    // In this case, get shell items only for file system items.
     hr = pfd->SetOptions(dwFlags | FOS_FORCEFILESYSTEM);
-
-    // Set the file types to display only.
-    // Notice that this is a 1-based array.
     hr = pfd->SetFileTypes(ARRAYSIZE(rgSpec), rgSpec);
-
-    // Set the selected file type index to Word Docs for this example.
-    hr = pfd->SetFileTypeIndex(0);
-
-    // Set the default extension to be ".doc" file.
+    hr = pfd->SetFileTypeIndex(1);
     hr = pfd->SetDefaultExtension(L"msh");
-    IShellItem *psiResult;
-
-    IShellItem *folder;
     hr = SHCreateItemFromParsingName(directory.wstring().c_str(), NULL, IID_PPV_ARGS(&folder));
     hr = pfd->SetDefaultFolder(folder);
 
     hr = pfd->Show(NULL);
-
-    IShellItem *psiResult;
     hr = pfd->GetResult(&psiResult);
-
-    PWSTR pszFilePath = NULL;
     hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
 
-    filename = std::filesystem::path(pszFilePath);
+    const auto path = std::filesystem::path(pszFilePath);
 
+    CoTaskMemFree(pszFilePath);
+    folder->Release();
     psiResult->Release();
-
     pfd->Release();
 
-    return filename;
+    return path;
 }
 
-// TODO: All the scenerios with expected outcomes !
-
-SCENARIO("Prompt save") {
-
-    GIVEN("An app with no file") {
-        App app("", false, false, false);
-
-        WHEN("Saving") {
-            app.Save();
-        }
-
-        WHEN("Saving As") {
-            app.SaveAs();
-        }
-
-        WHEN("Openning file") {
-            app.Open();
-        }
-
-        WHEN("Creating a new file") {
-            app.New();
-        }
-    }
-
-    GIVEN("An app with an new file") {
-        App app("unnamed.msh", false, true, false);
-
-        WHEN("Saving") {
-            app.Save();
-        }
-
-        WHEN("Saving As") {
-            app.SaveAs();
-        }
-
-        WHEN("Openning file") {
-            app.Open();
-        }
-
-        WHEN("Creating a new file") {
-            app.New();
-        }
-    }
-
-    GIVEN("An app with a unsaved file") {
-        App app("unnamed.msh", false, true, false);
-
-        WHEN("Saving") {
-            app.Save();
-        }
-
-        WHEN("Saving As") {
-            app.SaveAs();
-        }
-
-        WHEN("Openning file") {
-            app.Open();
-        }
-
-        WHEN("Creating a new file") {
-            app.New();
-        }
-    }
-
-    GIVEN("An app with a saved file") {
-        App app("super.msh", false, true, true);
-
-        WHEN("Saving") {
-            app.Save();
-        }
-
-        WHEN("Saving As") {
-            app.SaveAs();
-        }
-
-        WHEN("Openning file") {
-            app.Open();
-        }
-
-        WHEN("Creating a new file") {
-            app.New();
-        }
-    }
-
-    // Should be impossible
-    GIVEN("An app with a saved unnamed file") {
-        App app("unnamed.msh", true, true, false);
-
-        WHEN("Saving") {
-            app.Save();
-        }
-
-        WHEN("Saving As") {
-            app.SaveAs();
-        }
-
-        WHEN("Openning file") {
-            app.Open();
-        }
-
-        WHEN("Creating a new file") {
-            app.New();
-        }
-    }
-}
+// TODO: Add the expected result ! (check for the bools, names, etc...)
